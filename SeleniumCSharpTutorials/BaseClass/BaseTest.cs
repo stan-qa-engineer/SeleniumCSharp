6c3fd9b5c006fe7f5121b22a90f954e5f77ea1d8
Stan Kondratenko <stan.qa.engineer@gmail.com>
2020-09-21 13:14:26 -0500

    Add project files.

---
+++ SeleniumCSharpTutorials/BaseClass/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCSharpTutorials.BaseClass
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Open()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://opensource-demo.orangehrmlive.com/";
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumCSharpTutorials/TestClass.cs
@@ -0,0 +1,38 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumCSharpTutorials.BaseClass;
+
+namespace SeleniumCSharpTutorials
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test, Category("Smoke Testing")]
+        public void TestMethod1()
+        {                        
+            IWebElement usernameTextField = driver.FindElement(By.Id("txtUsername"));
+            usernameTextField.SendKeys("Admin");            
+        }
+
+        [Test, Category("Regression Testing")]
+        public void TestMethod2()
+        {
+            IWebElement usernameTextField = driver.FindElement(By.Id("txtUsername"));
+            usernameTextField.SendKeys("Admin");
+        }
+
+        [Test, Category("Smoke Testing")]
+        public void TestMethod3()
+        {
+            IWebElement usernameTextField = driver.FindElement(By.Id("txtUsername"));
+            usernameTextField.SendKeys("Admin");
+            Thread.Sleep(5000);
+        }
+    }
+}
